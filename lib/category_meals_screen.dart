68e55e7eae47f593fcb6d5bf43486787a280571a
baradipavan <64634366+baradipavan326-dev@users.noreply.github.com>
2020-12-23 16:08:05 +0530

    created a dummy meals and added content to it

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -16,16 +16,9 @@ class CategoryMealsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text(
-          'The Recipe For ' + categoryId,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
+      body: ListView.builder(itemBuilder: (ctx, index){
+
+      },itemCount: ,)
     );
   }
 }
