25a5aad1be62af5c8202d57344ecc90dee3219cf
baradipavan <64634366+baradipavan326-dev@users.noreply.github.com>
2020-12-23 16:09:48 +0530

    new dummy data added(ignore previous dummy data)

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/models/meal.dart';
-import './models/category.dart';
 
+import './models/category.dart';
+import './models/meal.dart';
 
 const DUMMY_CATEGORIES = const [
   Category(
@@ -56,9 +56,7 @@ const DUMMY_CATEGORIES = const [
   ),
 ];
 
-
-
- const DUMMY_MEALS = const [
+const DUMMY_MEALS = const [
   Meal(
     id: 'm1',
     categories: [
