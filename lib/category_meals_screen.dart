429ae880fb6676d0386f0710d2dcb66f789866fc
baradipavan <64634366+baradipavan326-dev@users.noreply.github.com>
2020-12-23 16:31:03 +0530

    added list builder in category_meals file

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -12,13 +13,21 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeal = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
-      appBar: AppBar(
-        title: Text(categoryTitle),
-      ),
-      body: ListView.builder(itemBuilder: (ctx, index){
-
-      },itemCount: ,)
-    );
+        appBar: AppBar(
+          title: Text(categoryTitle),
+        ),
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Text(
+              categoryMeal[index].title,
+              style: TextStyle(color: Colors.red, fontSize: 20),
+            );
+          },
+          itemCount: categoryMeal.length,
+        ));
   }
 }
