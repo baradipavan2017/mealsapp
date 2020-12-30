178a5dba89bf4486781af92825bddbc76ca0e57c
baradipavan <64634366+baradipavan326-dev@users.noreply.github.com>
2020-12-30 19:13:30 +0530

    added drawer to the main screen and added listtiles to drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/screens/categories_screen.dart';
-import 'package:meals_app/screens/favorites_screen.dart';
+import './categories_screen.dart';
+import './favorites_screen.dart';
+import '../widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -26,6 +27,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title']),
       ),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/screens/filters_screen.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget buildListTile(String title, IconData icon, Function typehandler) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: typehandler,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Colors.orange[700],
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Colors.yellow,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          buildListTile(
+            'Meals',
+            Icons.restaurant,
+            () {
+              Navigator.of(context).pushNamed('/');
+            },
+          ),
+          buildListTile(
+            'Filters',
+            Icons.settings_applications,
+            () {
+              Navigator.of(context).pushNamed(FiltersScreen.routeName);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
